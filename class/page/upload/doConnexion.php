245b8ea71a02c9f96452bcc90772858cbadf03e4
loclamor <favier.loic.31@gmail.com>
2013-02-26 17:21:25 +0100

    ajout d'un notify d'échec de connection

--- class/page/upload/doConnexion.php
+++ class/page/upload/doConnexion.php
@@ -5,7 +5,9 @@ class Page_Upload_DoConnexion extends Page {
 	public function controller($mixed) {
 		$pseudo = $mixed['pseudo'];
 		$pwd = $mixed['pwd'];
-		//TODO code de connexion à revoir : le parcours de tous les utilisateurs n'est pas viable si le site grossi
+		
+		$urlAction = new Url(true);
+		$urlAction->addParam('page', 'accueil');
 		
 		$log = new Logger('./logs');
 		
@@ -15,45 +17,19 @@ class Page_Upload_DoConnexion extends Page {
 			$_SESSION['upload']['isConnect'] = 'true';
 			$_SESSION['upload']['id'] = $users[0]->getId();
 			$log->log('success', 'success_connection', 'Success de connection pour '.$pseudo, Logger::GRAN_MONTH);
+			$urlAction->addParam('notify', 'Bienvenue '.$pseudo);
 		}
 		else {
 			unset($_SESSION['upload']['isConnect']);
 			unset($_SESSION['upload']['id']);
 			$log->log('erreurs', 'erreurs_connection', 'Echec de connection pour '.$pseudo.' '.$pwd, Logger::GRAN_MONTH);
+			$urlAction->addParam('notify', 'Echec de connection pour '.$pseudo);
 		}
 		
-		/*
-		$userExist = false;
-		$userId = false;
-		if(!is_null($pseudo) and !is_null($pwd)){
-			$users = Gestionnaire::getGestionnaire('Utilisateur')->getAll();
-			foreach ($users as $user) {
-				if($user instanceof Utilisateur) {
-					if($user->getPseudo() == $pseudo and $user->getMotDePasse() == $pwd) {
-						$userExist = true;
-						$userId = $user->getId();
-					}
-				}
-			}
-			if($userExist) {
-				$_SESSION['upload']['isConnect'] = 'true';
-				$_SESSION['upload']['id'] = $userId;
-			}
-			else {
-				
-			}
-		}
-		else {
-			unset($_SESSION['upload']['isConnect']);
-			unset($_SESSION['upload']['id']);
-		}
 		
-		*/
 		
-		$urlAction = new Url(true);
-		$urlAction->addParam('page', 'accueil');
-		$urlAction->addParam('notify', 'Bienvenue '.$pseudo);
+		
 		redirect($urlAction->getUrl());
-		echo 'Vous avez été connecté.<br/><a href="'.$urlAction->getUrl().'">retour accueil</a>';
+		echo 'Vous avez ï¿½tï¿½ connectï¿½.<br/><a href="'.$urlAction->getUrl().'">retour accueil</a>';
 	}
 }
